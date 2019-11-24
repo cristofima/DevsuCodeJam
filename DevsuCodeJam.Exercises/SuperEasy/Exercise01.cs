1d09b59a8680a3df32473eb0f2005b47c56e09e4
Cristopher Coronado Moreira <cristofima@hotmail.com>
2019-11-24 17:36:08 -0500

    Init

---
+++ DevsuCodeJam.Exercises/SuperEasy/Exercise01.cs
@@ -0,0 +1,36 @@
+﻿namespace DevsuCodeJam.Exercises.SuperEasy
+{
+    public class Exercise01
+    {
+        public static string Encrypt(string key, string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                return string.Empty;
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                key = "DCJ";
+            }
+
+            string result = "";
+
+            char[] array = message.ToCharArray();
+            string itemAux;
+
+            foreach (char item in array)
+            {
+                itemAux = item.ToString().ToLower();
+                if (itemAux == "a" || itemAux == "e" || itemAux == "i" || itemAux == "o" || itemAux == "u")
+                {
+                    result += key;
+                }
+
+                result += item;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DevsuCodeJam.Tests/SuperEasyTests.cs
@@ -0,0 +1,19 @@
+﻿using DevsuCodeJam.Exercises.SuperEasy;
+using Xunit;
+
+namespace DevsuCodeJam.Tests
+{
+    public class SuperEasyTests
+    {
+        [Theory]
+        [InlineData("dcjI ldcjovdcje prdcjOgrdcjAmmdcjing!", "dcj", "I love prOgrAmming!")]
+        [InlineData("", "dcj", null)]
+        [InlineData("HDCJellDCJo!", null, "Hello!")]
+        public void Exercise01_Test(string resultExpected, string key, string message)
+        {
+            var result = Exercise01.Encrypt(key, message);
+
+            Assert.Equal(resultExpected, result);
+        }
+    }
+}
